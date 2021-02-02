09e623651e3194a7ac8878b37fdedb03d39a4f61
Achin Kulshreshtha <61320638+StarkSoul@users.noreply.github.com>
2021-02-02 14:59:58 +0530

    updating home page button

--- src/components/loader.js
+++ src/components/loader.js
@@ -8,7 +8,7 @@ import { IconLoader } from '@components/icons';
 const StyledLoader = styled.div`
   ${({ theme }) => theme.mixins.flexCenter};
   position: fixed;
-  top: 5;
+  top: 0;
   bottom: 0;
   left: 0;
   right: 0;
